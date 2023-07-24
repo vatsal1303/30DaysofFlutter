2fa4fcbe2967744c22897174afd1830bbad2ff2d
Vatsal <vatsal.raj1234@gmail.com>
2023-07-24 19:04:22 +0530

    Day 3 completed

---
+++ lib/Home_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  final days = 1;
+  final name = "Vatsal";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("V Shooping"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("welcome $days $name"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
- import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_catalog/Home_page.dart';
 
- void main(){
-  runApp(MyApp()); 
- }
-
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -11,13 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("welcome"), 
-          ),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
